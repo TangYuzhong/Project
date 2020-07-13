86802b94f4df76f98921eb3c882a52476825a373
guobin@163.com <1353510670@qq.com>
2020-07-13 16:38:15 +0800

    修正了无用表格结构，添加符合需要的用户实体类

---
+++ src/main/java/com/ssm/project/entity/User.java
@@ -0,0 +1,15 @@
+package com.ssm.project.entity;
+
+import javax.xml.crypto.Data;
+
+public class User {
+    private Integer uid;
+    private String username;
+    private String password;
+    private String idCard;
+    private Data createTime;
+    private Data updateTime;
+    private Integer status;
+    private Integer isMer;
+
+}
